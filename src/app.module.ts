cb8fdca906477840b9f0ee531ea8a5ab942fc1a7
james jhong <jamesjhong@star-link.tech>
2025-10-05 18:04:54 +0800

    feat: 🎸 test configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TodoModule } from './features/todo/todo.module';
+import serverConfig from './configs/server.config';
+import databaseConfig from './configs/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['dev.local.env', 'dev.env'],
+      load: [serverConfig, databaseConfig],
+      expandVariables: true,
+    }),
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  constructor(private readonly configService: ConfigService) {}
+  onModuleInit(): void {
+    const { host, password } = this.configService.get('database');
+    const domain = this.configService.get('APP_DOMAIN');
+    const redirect = this.configService.get('APP_REDIRECT_URL');
+    console.log(host, password, domain, redirect);
+  }
+}
