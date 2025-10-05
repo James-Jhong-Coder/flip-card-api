43b03508fdef5076cb110db2115a87986c7f42b1
james jhong <jamesjhong@star-link.tech>
2025-10-05 22:01:15 +0800

    feat: 🎸 練習動態模組功能

---
+++ src/modules/hello/hello.service.ts
@@ -0,0 +1,9 @@
+import { Inject, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class HelloService {
+  constructor(@Inject('INPUT') private readonly input: string) {}
+  public sayHello() {
+    return `Hello ${this.input}`;
+  }
+}
