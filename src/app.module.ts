3101c574f2cd5064b7bfb0b2458a97c8cdf96203
james jhong <jamesjhong@star-link.tech>
2025-10-06 13:17:51 +0800

    feat: 🎸 add middleware practice

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './middlewares/logger/logger.middleware';
+import { HelloMiddleware } from './middlewares/hello/hello.middleware';
+import { TodoModule } from './features/todo/todo.module';
+import { TodoController } from './features/todo/todo.controller';
 
 @Module({
-  imports: [],
+  imports: [TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+    consumer.apply(HelloMiddleware).forRoutes(TodoController);
+  }
+}
