449c3265436585cdb681ef845b5e13fa4074b3a9
JamesJhong <jamesjhong@star-link.tech>
2025-10-16 06:26:06 +0800

    feat: 🎸 add user profile api

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { UsersService } from './users.service';
 
+@UseGuards(JwtAuthGuard)
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Get('profile')
+  async getProfile(@Req() req: any) {
+    const userId = Number(req.user.id);
+    const userData = await this.userService.findById(userId);
+    return {
+      name: userData?.name,
+      email: userData?.email,
+    };
+  }
+}
