447ed1b645fd5a699a920f709362b962acbd5eac
james jhong <jamesjhong@star-link.tech>
2025-10-19 16:38:40 +0800

    feat: 🎸 修改卡片時 卡片 id 要用數字

--- src/modules/flash-card/dto/update-card.dto.ts
+++ src/modules/flash-card/dto/update-card.dto.ts
@@ -1,8 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCardDto } from './create-card.dto';
-import { IsString } from 'class-validator';
+import { IsNumber } from 'class-validator';
 
 export class UpdateCardDto extends PartialType(CreateCardDto) {
-  @IsString()
+  @IsNumber()
   cardId: number;
 }
