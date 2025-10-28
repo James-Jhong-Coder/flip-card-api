ca265de05cffce7f2a98fa487b77fb3b198c555e
james jhong <jamesjhong@star-link.tech>
2025-10-28 18:56:58 +0800

    feat: 🎸 add new cors white list

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://192.168.66.21:5173', 'http://10.1.201.77:5173'],
+    origin: ['http://192.168.66.21:5173', 'http://10.1.201.77:5173', 'http://192.168.66.21:5174'],
   });
   await app.listen(process.env.PORT ?? 3000);
 }
