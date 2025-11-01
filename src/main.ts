5eeb4445636843329c3b6077d0c9a5ab05a7480f
james jhong <jamesjhong@star-link.tech>
2025-11-01 11:29:17 +0800

    feat: 🎸 add cors test

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    // origin: [
-    //   'http://192.168.66.21:5173',
-    //   'http://10.1.201.77:5173',
-    //   'http://192.168.66.21:5174',
-    //   'https://flip-card-ui.vercel.app',
-    // ],
-  });
+  app.enableCors();
+  // app.enableCors({
+  // origin: true,
+  // methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  // allowedHeaders: ['Content-Type', 'Authorization'],
+  // origin: [
+  //   'http://192.168.66.21:5173',
+  //   'http://10.1.201.77:5173',
+  //   'http://192.168.66.21:5174',
+  //   'https://flip-card-ui.vercel.app',
+  // ],
+  // });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
