15040ea2152c025f56629bd06d12b3599e281585
james jhong <jamesjhong@star-link.tech>
2025-11-01 10:43:27 +0800

    feat: 🎸 add cors origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://192.168.66.21:5173', 'http://10.1.201.77:5173', 'http://192.168.66.21:5174'],
+    origin: [
+      'http://192.168.66.21:5173',
+      'http://10.1.201.77:5173',
+      'http://192.168.66.21:5174',
+      'https://flip-card-ui.vercel.app',
+    ],
   });
   await app.listen(process.env.PORT ?? 3000);
 }
